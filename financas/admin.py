8d4b67a03b85560af4d7f34fc86d74278f8387e8
Guilherme Canedo <guilhermecanedo09@gmail.com>
2025-06-16 17:30:38 -0300

    feat: adicionando modelos ao arquivo admin.py para criação de painel de administração do django

--- financas/admin.py
+++ financas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categoria, Conta, Movimentacao, Planejamento, ListaDeCompras, ItemListaDeCompras
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Conta)
+admin.site.register(Movimentacao)
+admin.site.register(Planejamento)
+admin.site.register(ListaDeCompras)
+admin.site.register(ItemListaDeCompras)
