e98c113cb6df7ff6633f01814713cb8afafa2333
Guilherme Canedo <guilhermecanedo09@gmail.com>
2025-06-23 17:43:07 -0300

    chore: alterando códigos de arquivos com funcionalidades deletadas.

--- financas/admin.py
+++ financas/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import Categoria, Conta, Movimentacao, Planejamento, ListaDeCompras, ItemListaDeCompras
+from .models import Categoria, Conta, Movimentacao
 
 admin.site.register(Categoria)
 admin.site.register(Conta)
 admin.site.register(Movimentacao)
-admin.site.register(Planejamento)
-admin.site.register(ListaDeCompras)
-admin.site.register(ItemListaDeCompras)
